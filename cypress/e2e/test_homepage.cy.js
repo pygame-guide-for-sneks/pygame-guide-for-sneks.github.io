a8cb2f06d5ee7804292df8bce6ad26b311351b76
Andrew Coffey <49015102+oddbookworm@users.noreply.github.com>
2022-12-27 22:58:17 -0600

    updated test to check all links

--- cypress/e2e/test_homepage.cy.js
+++ cypress/e2e/test_homepage.cy.js
@@ -4,27 +4,53 @@ describe ('Home Page Testing', () => {
     cy.contains("Pygame Guide for Sneks");
   });
 
-  const pages = [
-    "Base setup and opening a window",
-    "Drawing shapes",
-    "Moving things",
-    "Loading images and drawing them",
-    "Using classes to better contain our code",
-    "Loading and playing audio files",
-    "Making a small game to demonstrate how these go together",
-    "Fonts in pygame",
-    "Colors in pygame",
-    "PixelArray",
-    "Blend modes",
-    "pygame.display.set_mode flags"
-  ];
+  /* 
+  Leaving this code here as an alternative test for only specific
+  links on a page.
+  */
 
-  pages.forEach(page => {
-    it (`Testing ${page}`, () => {
-      cy.visit("/");
-      cy.contains(page).then(link => {
-        cy.request({ url: link.prop('href') });
-      });
-    });
+  // const pages = [
+  //   "Base setup and opening a window",
+  //   "Drawing shapes",
+  //   "Moving things",
+  //   "Loading images and drawing them",
+  //   "Using classes to better contain our code",
+  //   "Loading and playing audio files",
+  //   "Making a small game to demonstrate how these go together",
+  //   "Fonts in pygame",
+  //   "Colors in pygame",
+  //   "PixelArray",
+  //   "Blend modes",
+  //   "pygame.display.set_mode flags"
+  // ];
+
+  // pages.forEach(page => {
+  //   it (`Testing ${page}`, () => {
+  //     cy.visit("/");
+  //     cy.contains(page).then(link => {
+  //       cy.request({
+  //         url: link.prop('href'),
+  //         failOnStatusCode: false
+  //       }).then((response) => {
+  //         expect(response.status).to.be.lt(300)
+  //       });
+  //     });
+  //   });
+  // });
+
+  it('check all links to sites', () => {
+    const failed_pages = []
+    cy.visit('/')
+    cy.get("a:not([href*='mailto:'])").each(page => {
+      cy.request({
+        url:page.prop('href'),
+        failOnStatusCode: false
+      }).then((response) => {
+        if (!response.isOkStatusCode) {
+          failed_pages.push({ url: page.prop('href').split("4000")[1], status: response.status })
+        }
+      })
+    })
+    .then(() => {expect(failed_pages).to.equal([])})
   });
 });
