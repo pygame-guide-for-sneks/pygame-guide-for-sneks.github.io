fa368fff7754d3639393652ef956424cb454a3f3
Andrew Coffey <49015102+oddbookworm@users.noreply.github.com>
2023-01-19 15:02:38 -0600

    Test Overhaul (#78)
    * added tests, refactored, and migrated to Page Object Model
    
    * removed random, seemingly useless anchor
    
    * made use of quotes consistent

---
+++ cypress/e2e/test_classpage.cy.js
@@ -0,0 +1,21 @@
+import { ClassPage } from "../pages/classPage";
+import { checkIfValid } from "../pages/homePage";
+
+describe ("Class Page Testing", () => {
+  let page = new ClassPage();
+  it("Visit the page", () => {
+    page.visit();
+    cy.contains("Pygame Guide for Sneks");
+  })
+
+  it("check all links to sites", () => {
+    let failed_pages = [];
+    page.visit();
+    page.getLinks().each(page => {
+      checkIfValid(page, failed_pages);
+    })
+    .then(() => {
+      expect(failed_pages).to.deep.equal([]);
+    })
+  });
+})
---
+++ cypress/pages/homePage.js
@@ -0,0 +1,30 @@
+class HomePage {
+  visit() {
+    cy.visit("/");
+  }
+
+  getLinks() {
+    return cy.get("a:not([href*=\"mailto:\"])");
+  }
+}
+
+function checkIfValid(page, failed_pages) {
+    let url = page.prop("href");
+    if (url == "https://pygame.org/docs") {
+        return;
+    }
+    cy.request({
+      url: url,
+      failOnStatusCode: false
+    }).then((response) => {
+      if (!response.isOkStatusCode) {
+        if (url.includes("localhost")) {
+            failed_pages.push({ url: url.split("4000")[1], status: response.status })
+        } else {
+            failed_pages.push({ url: url, status: response.status })
+        }
+      }
+    })
+  }
+
+export { HomePage, checkIfValid };
